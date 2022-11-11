f1e75329dd821e3b8a07db0945cba2a6578b89bb
Jack Eadie <jack@marqo.ai>
2022-11-11 15:03:33 +1000

    stars.sh as cmd in Go

--- cmd/root.go
+++ cmd/root.go
@@ -2,22 +2,36 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/google/go-github/v48/github"
 	"github.com/spf13/cobra"
 	"os"
 )
 
+//TODO: rate limiting
+// repos, _, err := client.Repositories.List(ctx, "", nil)
+// if _, ok := err.(*github.RateLimitError); ok {
+//	log.Println("hit rate limit")
+// }
+// https://github.com/google/go-github#rate-limiting
+
+// TODO: default to conditional requests to reduce use of rate limits
+//   https://docs.github.com/en/rest/overview/resources-in-the-rest-api#conditional-requests
+
 var (
+	client  *github.Client
 	rootCmd = &cobra.Command{
 		Use:   "repo-analytics",
 		Short: "Analytics for your Github repository",
 		Long:  `Analytics for your Github repository`,
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("Hello world")
+			fmt.Println("root")
 		},
 	}
 )
 
-//func init() { }
+func init() {
+	client = github.NewClient(nil)
+}
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
--- cmd/starrers.go
+++ cmd/starrers.go
@@ -1,17 +1,54 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"github.com/google/go-github/v48/github"
 	"github.com/spf13/cobra"
+	"io"
+	"os"
 )
 
+func outputStargazers(gazers []*github.Stargazer, fp io.Writer) {
+	for _, g := range gazers {
+		_, err := fmt.Fprintf(fp, "%s\n", *g.User.Login)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to output stargazer details, %s\n", err.Error())
+		}
+	}
+}
+
 var (
+	owner       string
+	repo        string
 	starrersCmd = &cobra.Command{
 		Use:   "starrers",
 		Short: "Get all stargazers from a Github repository",
 		Long:  `Get all stargazers from a Github repository`,
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("starrers")
+			if len(args) < 2 {
+				fmt.Fprintf(os.Stderr, "Did not provide owner and repo as argv")
+				return
+			}
+			owner = args[0]
+			repo = args[1]
+
+			var starrers []*github.Stargazer
+			opts := &github.ListOptions{PerPage: 50}
+
+			for {
+				stargazers, resp, err := client.Activity.ListStargazers(context.TODO(), owner, repo, opts)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "%s\n", err)
+					return
+				}
+				starrers = append(starrers, stargazers...)
+				if resp.NextPage == 0 {
+					break
+				}
+				opts.Page = resp.NextPage
+			}
+			outputStargazers(starrers, os.Stdout)
 		},
 	}
 )
